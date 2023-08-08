dfab80d86713811444c2431a5cd4e3a35466f198
Sergio Souza <serjsouza93@gmail.com>
2023-08-08 17:51:03 -0300

    css adjusts grid created

---
+++ components/Grid.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Grid = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  )
+}
+
+export default Grid
